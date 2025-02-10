a182f690fada037ba01e033c4b0573e6230947b9
Gabriela Otte <gabriela@otte.net.br>
2025-02-10 17:21:13 -0300

    Adição de função que retorna os maiores valores e as imagens correspondentes para cada métrica

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from collections import defaultdict
 
 num_casas_decimais = 2
 
@@ -13,4 +14,16 @@ def save_table(df, diretorio):
     path = os.path.join(diretorio, "compare_table")
     df.to_csv(path, index=False)
 
-    
+def max_values(dados):
+    max_values = defaultdict(list)
+    df = create_dataframe(dados)
+    for metric in df.columns:
+        if metric == "Imagem":
+            continue
+        index = df[metric].idxmax()
+        max_values[metric].append(dados["Imagem"][index])
+        max_values[metric].append(dados[metric][index])
+    df_max_values = create_dataframe(max_values)
+    return df_max_values
+
+
