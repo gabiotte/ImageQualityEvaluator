cf7fb8306d141b506f8f98dcdbba630ad755a348
Gabriela Otte <gabriela@otte.net.br>
2025-05-15 14:34:55 -0300

    Ajustes no código e remoção de funções não utilizadas

--- compare.py
+++ compare.py
@@ -3,52 +3,53 @@ from collections import defaultdict
 from Image import *
 import pandas as pd
 
-def compare_group_images(group_path, group):
+def create_compare_table(dir):
     results = defaultdict(list)
 
     allowed_ext = (".jpg", ".jpeg", ".bmp")
 
-    capture_time_path = os.path.join(group_path, "tempo.csv")
-    capture_time_df = pd.read_csv(capture_time_path)
-    capture_time_df.set_index("Imagem", inplace=True)
+    # Abre os arquivos csv
+    selecoes_path = os.path.join(dir, "selecoes.csv")
+    capture_time_path = os.path.join(dir, "tempo.csv")
+
+    # Cria o DF do tempo de captura
+    if os.path.exists(capture_time_path):
+        capture_time_df = pd.read_csv(capture_time_path)
+        capture_time_df.set_index("Imagem", inplace=True)
     
-    for file in os.listdir(group_path):
-        path = os.path.join(group_path, file)
+    # Cria o DF das coordenadas do ruído
+    selecoes_df = pd.read_csv(selecoes_path)
+    selecoes_df.set_index("Imagem", inplace=True)
+
+    # Abre todos os arquivos de imagem
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
 
         if not file.lower().endswith(allowed_ext):
             continue
+        
+        # Pega as coordenadas do ruído
+        linha = selecoes_df.loc[file]
+        x, y, w, h = int(linha["x"]), int(linha["y"]), int(linha["w"]), int(linha["h"])
+        
+        # Instancia o objeto imagem
+        image = Image(path, x, y, w, h)
 
-        image = Image(path)
-
-        results["Grupo"].append(group)
+        # Adiciona os resultados em um DF
         results["Imagem"].append(file)
-        results["Desfoque"].append(image.blur())
+        results["Nitidez"].append(image.blur())
         results["Contraste"].append(image.contrast())
         results["Ruído"].append(image.noise())
-        results["Tempo de Captura (µs)"].append(capture_time_df.loc[file," Tempo (µs)"])
-            
+        if os.path.exists(capture_time_path):
+            results["Tempo de Captura (µs)"].append(capture_time_df.loc[file," Tempo (µs)"])
+    
+    # Arredonda os valores e retorna o DF
     results_df = pd.DataFrame(results).round(2)
     return results_df
 
-def camera_table(camera_path):
-    table_df = pd.DataFrame()
+def save(dir, dataframe):
+    path = os.path.join(dir,"comparacao.xlsx")
 
-    for group in os.listdir(camera_path):
-        
-        group_path = os.path.join(camera_path, group)
-        if not os.path.isdir(group_path):
-            continue
-
-        group_results_df = compare_group_images(group_path, group)
-
-        table_df = pd.concat([group_results_df, table_df])
-    
-    table_path = os.path.join(camera_path,"comparacao.xlsx")
-    save(table_df,table_path)
-
-    return table_df
-
-def save(dataframe, path):
-    #dataframe.to_csv(path)
+    # Salva o DF em um arquivo excel
     with pd.ExcelWriter(path) as writer:
         dataframe.to_excel(writer, index=False)
